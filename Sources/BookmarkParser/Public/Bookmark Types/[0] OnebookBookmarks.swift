eb4a95c14a6c3244748486e0b5cbeb0b5546d472
luardemin <luardemin@gmail.com>
2021-10-06 19:57:00 +0900

    Edit: made onebook bookmark types codable

--- Sources/BookmarkParser/Public/Bookmark Types/[0] OnebookBookmarks.swift
+++ Sources/BookmarkParser/Public/Bookmark Types/[0] OnebookBookmarks.swift
@@ -1,10 +1,10 @@
-public struct OnebookChildren {
+public struct OnebookChildren: Codable {
     let name: String
     let url: String?
     let folders: [OnebookChildren?]
     let bookmarks: [OnebookChildren?]
 }
 
-public struct OnebookBookmarks {
+public struct OnebookBookmarks: Codable {
     let folders: [OnebookChildren?]
 }
