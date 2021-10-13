ef6c2160138dc3a52adbe54f5237ba848847dcb8
luardemin <luardemin@gmail.com>
2021-10-14 00:05:13 +0900

    Changes for potential direct Chromium bookmark creation

--- Sources/BookmarkParser/Bookmark Types/[1] ChromiumBookmarks.swift
+++ Sources/BookmarkParser/Bookmark Types/[1] ChromiumBookmarks.swift
@@ -15,18 +15,29 @@ public struct Roots: Codable {
 public struct ChromiumChildren: OnebookItem, Codable {
     public typealias ItemType = ChromiumChildren
 
-    public let name: String
-    public let url: String?
     public let children: [ItemType]?
     // let date_added: String
+    // let date_modified: String?
     // let guid: String
     // let id: String
+    public let name: String
     // let type: String
+    public let url: String?
 
     public init(name: String, url: String?, children: [ItemType]?) {
         self.name = name
         self.url = url
         self.children = children
+        // self.date_added = "\(Int(Date().timeIntervalSince1970 * 1000))"
+        // self.guid = UUID().uuidString.lowercased()
+
+        // if let _ = url {
+        //     self.type = "url"
+        //     self.date_modified = nil
+        // } else {
+        //     self.type = "folder"
+        //     self.date_modified = "\(Int(Date().timeIntervalSince1970 * 1000))"
+        // }
     }
 }
 
